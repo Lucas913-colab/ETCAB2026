e6faa17424294d6f34e1585d6ed23bec807c1c23
Lucas <lucasroque0802@gmail.com>
2026-03-13 22:15:37 -0300

    concluido lição 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Lucas roque");
+Console.WriteLine("ola usuário, meu nome é Lucas");
+Console.WriteLine("ola, Santos");
+Console.WriteLine("ola, São vicente");
+Console.WriteLine("ola, Guarujá");
+Console.WriteLine("ola, Praia Grande");
+Console.WriteLine("ola, Mongaguá");
+Console.WriteLine("ola, itanhaem");
+Console.WriteLine("ola, Peruibe");
+Console.WriteLine("ola, Iguape");
+Console.WriteLine("ola, Ilha-Cumprida");
+Console.WriteLine("ola, Cananéia");
